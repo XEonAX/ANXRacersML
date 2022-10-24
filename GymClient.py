a8614ced818b6d64d114c6095bb5f1fe6a5763ab
XEonAX <sv.xeon@gmail.com>
2022-10-24 22:57:23 +0530

    Added mmap structure

--- GymClient.py
+++ GymClient.py
@@ -8,6 +8,73 @@ import uuid
 
 from GameState import GameState
 
+    # <MMAP>
+    # 0-100 (Unicode) MemoryMapName = "ANXRacersGymInterface"
+    # 100-200 (Object) Level
+    #     100-116 (Guid) LevelId
+    #     116-120 (int) Number of Checkpoints
+    #     120-124 (int) Laps
+    #     124-128 (float) Difficulty
+    #     128- (Unicode) LevelName
+    # 200-300 (Object) Spaceship Physics
+    #     200-216 (Guid) LevelId
+    #     216-220 (float) Mass         
+    #     220-224 (float) LDrag        
+    #     224-228 (float) ADrag        
+    #     228-232 (float) SurgeForward 
+    #     232-236 (float) SurgeBackward
+    #     236-240 (float) Strafe       
+    #     240-244 (float) Torque       
+    #     244-248 (float) Radius       
+    #     248-252 (float) Friction     
+    #     252-256 (float) Bounce  
+    #     256- (Unicode) ShipName     
+    # 300-400 (Object) Player
+    #     300-316 (Guid) UserId
+    #     316- (Unicode) DisplayName
+    # 400-404 (int) GameState
+    # 404-410 Empty
+    # 410-418 (Vector2) Spaceship Inputs
+    #     410-414 (float) Y Surge +1.0 -1.0
+    #     414-418 (float) Z Turn +1.0 -1.0
+    # 418-422 (int) Reset Input    
+    # 422-426 (int) Giveup Input    
+    # 426-512 Empty
+    # 512-516 (int) UpdateNumber
+    # 520-556 (Object) Spaceship State Live
+    #     520-528 (Vector2) Spaceship Position
+    #         520-524 (float) X Position
+    #         524-528 (float) Y Position
+    #     528-536 (Vector2) Spaceship Rotation
+    #         528-532 (float) Z Rotation
+    #         532-536 (float) W Rotation
+    #     536-544 (Vector3) Spaceship Rigidbody
+    #         536-540 (float) X Velocity
+    #         540-544 (float) Y Velocity
+    #         544-548 (float) Z Angular Velocity
+    #     548-556 (Vector3) Spaceship Inputs
+    #         548-552 (float) Y Surge +1.0 -1.0
+    #         552-556 (float) Z Turn +1.0 -1.0
+    # 556-560 Empty    
+    # 560-580 (Object) Track State Live
+    #     560-568 (Vector2) Rel Position
+    #         560-564 (float) X Position
+    #         564-568 (float) Y Position
+    #     568-576 (Vector2) Rotation
+    #         568-572 (float) Z Rotation
+    #         572-576 (float) W Rotation
+    #     576-580 (int) Checkpoint Index
+    # 600 (Array * 24 * 5) Sensor Info Live (24 Sensors returning 5 Detections each)
+    #     0 (bool) Hit
+    #     1 (bool) IsObstacle
+    #     2 (bool) IsProp
+    #     3 (bool) IsTrack
+    #     4-8 (int) ObjType
+    #     8-12 (float) Distance
+    #     12-20 (Vector2) Rotation
+    #         12-16 (float) Z Rotation
+    #         16-20 (float) W Rotation
+    # </MMAP>
 
 class ANXRacersTelemetryClient:
     def __init__(self, tagname="ANXRacersGymInterface"):
